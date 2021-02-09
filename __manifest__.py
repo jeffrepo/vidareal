a5aa251f53cc47f44a7b21c07557ca63718930a3
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2021-02-09 02:31:19 -0600

    init

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Vida Real',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'Vida Real',
+    'description': """ Vida Real """,
+    'website': 'http://aquih.com',
+    'author': 'aquíH',
+    'depends': ['account'],
+    'data': [
+        'views/report.xml',
+     	'views/reporte_payment1.xml',
+      	'views/reporte_payment2.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
